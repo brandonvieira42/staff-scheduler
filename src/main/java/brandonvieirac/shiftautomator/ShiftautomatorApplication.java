100628033baaf9d9f884703b81d134efa6faf5fb
BrandonVieira-C <brandon.c.vieira@gmail.com>
2023-02-03 13:17:19 -0500

    first commit

---
+++ src/main/java/brandonvieirac/shiftautomator/ShiftautomatorApplication.java
@@ -0,0 +1,13 @@
+package brandonvieirac.shiftautomator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftautomatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftautomatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/brandonvieirac/shiftautomator/ShiftautomatorApplicationTests.java
@@ -0,0 +1,13 @@
+package brandonvieirac.shiftautomator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftautomatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
